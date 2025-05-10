a07d7c87db5c826e1903e77d72eb032371ab9cae
shivalika-jpg <shivalikaa.sinha2003@gmail.com>
2025-05-10 14:20:25 +0530

    Create authRoutes.js

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+
+const authController = require('../controllers/authController');
+
+
+console.log('Loaded controller:', authController);
+console.log('register:', authController.register);
+console.log('login:', authController.login);
+
+
+router.post('/register', authController.register);
+router.post('/login', authController.login);
+
+module.exports = router;
