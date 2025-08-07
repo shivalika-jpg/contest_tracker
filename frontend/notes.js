7b2fbbacc3c3f1eed2bee0d82a936d231c13c4ec
shivalika-jpg <shivalikaa.sinha2003@gmail.com>
2025-08-08 02:13:47 +0530

    Update notes.js

--- frontend/notes.js
+++ frontend/notes.js
@@ -21,11 +21,7 @@ form.addEventListener('submit', async (e) => {
     problemsSolved: solvedInput.value,
     notes: noteInput.value
   };
-
-<<<<<<< HEAD
 const apiUrl = window.location.hostname === 'localhost' ? 'http://localhost:5000' : 'https://contest-tracker-qhfl.onrender.com';
-=======
->>>>>>> 1b93c6e (WIP: pre-rebase commit (cleaned, removed .env))
   const res = await fetch(`${apiUrl}/api/contest-log`, {
     method: 'POST',
     headers: {
