d4d3bce7035c04a3a0988841e961286477651e15
shivalika-jpg <shivalikaa.sinha2003@gmail.com>
2025-08-08 02:09:56 +0530

    Update contests.js

--- frontend/contests.js
+++ frontend/contests.js
@@ -4,12 +4,7 @@ if (!token) {
   alert('Please log in first.');
   window.location.href = 'login.html';
 }
-
-<<<<<<< HEAD
 const apiUrl = window.location.hostname === 'localhost' ? 'http://localhost:5000' : 'https://contest-tracker-qhfl.onrender.com';
-=======
-const apiUrl = window.location.hostname === 'localhost' ? 'http://localhost:5000' : window.location.origin;
->>>>>>> 1b93c6e (WIP: pre-rebase commit (cleaned, removed .env))
 fetch(`${apiUrl}/api/contests/codeforces`, {
   headers: {
     Authorization: `Bearer ${token}`
