fe84850e3ea90c1d38bebc3fc8e07fa22597844c
shivalika-jpg <shivalikaa.sinha2003@gmail.com>
2025-09-04 20:48:01 +0530

    Fix API URL configuration to use Vercel deployment instead of Render

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,6 @@
 // Configuration file for frontend
 window.config = {
   apiUrl: window.location.hostname === 'localhost' 
-    ? 'http://localhost:5000' 
-    : 'https://contest-tracker-qhfl.onrender.com'
+    ? 'http://localhost:5001' 
+    : window.location.origin
 };
